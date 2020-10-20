1da5504ed0e31228c597bf640601e1de8dc58829
your name here <johnobrien8642@gmail.com>
2020-10-20 15:38:55 -0400

    ::UsersController #destroy, #update, #show, #user_params created and working

--- First_Routes_And_Controllers/app/controllers/users_controller.rb
+++ First_Routes_And_Controllers/app/controllers/users_controller.rb
@@ -4,11 +4,42 @@ class UsersController < ApplicationController
     end
 
     def create
-      render json: params
+      user = User.new(user_params)
+      if user.save!
+        render json: user
+      else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+      end
     end
 
     def show
-      render json: params
+      render json: User.find(params[:id])
+    end
+
+    def update
+      user = User.find(params[:id])
+
+      if user.update_attributes(user_params)
+        render json: user
+      else
+        render json: user.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      user = User.find(params[:id])
+      if user.destroy
+        render plain: 'user was deleted'
+      else
+        render user.errors.full_messages, status: :unprocessable_entity
+      end
+    end
+
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email)
     end
 
 end

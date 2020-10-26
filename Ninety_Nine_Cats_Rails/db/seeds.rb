38b84aeeaa3544949833173e661246aaa74d936e
your name here <johnobrien8642@gmail.com>
2020-10-26 18:33:03 -0400

    index.html.erb and show.html.erb created, both working, returning all cats and selected cat respectively, CatsController#index and #show created

--- Ninety_Nine_Cats_Rails/db/seeds.rb
+++ Ninety_Nine_Cats_Rails/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+c1 = Cat.create(birth_date: "Nov 4th 2015", color: "brown", 
+                name: "Sprinkles", sex: "F", 
+                description: "A nice cat, probably too nice"
+            )
+c2 = Cat.create(birth_date: "Nov 5th 2015", color: "white", 
+                name: "Cinnamon", sex: "F", 
+                description: "This cat loves to jump"
+            )
+c3 = Cat.create(birth_date: "Nov 6th 2016", color: "black", 
+                name: "Furball", sex: "M", 
+                description: "Man this cat sure has a lot of fur"
+            )
+c4 = Cat.create(birth_date: "Nov 7th 2011", color: "orange", 
+                name: "Sprinkles", sex: "F", 
+                description: "A nice cat, probably too nice"
+            )
+c5 = Cat.create(birth_date: "Jan 1st 2018", color: "brown", 
+                name: "Cato", sex: "M", 
+                description: "A young vibrant cat"
+            )

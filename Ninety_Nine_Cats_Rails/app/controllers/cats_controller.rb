8f856369bf6c6c525e09406f515b5b8158c80e64
your name here <johnobrien8642@gmail.com>
2020-10-26 21:41:23 -0400

    edit.html.erb ::CatsController #new, #create, #edit, #update created

--- Ninety_Nine_Cats_Rails/app/controllers/cats_controller.rb
+++ Ninety_Nine_Cats_Rails/app/controllers/cats_controller.rb
@@ -6,6 +6,47 @@ class CatsController < ApplicationController
 
   def show
     @cat = Cat.find(params[:id])
-    render :show
+
+    if @cat
+      render :show
+    else
+      redirect_to cats_url
+    end
+  end
+
+  def new
+    render :new
+  end
+
+  def create
+    @cat = Cat.new(cat_params)
+
+    if @cat.save!
+      redirect_to cat_url(@cat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @cat = Cat.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @cat = Cat.find(params[:id])
+    if @cat.update_attributes(cat_params)
+      redirect_to cat_url(@cat)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def cat_params
+    params.require(:cats).permit(:age, :birth_date, :color, 
+      :name, :sex, :description
+      )
   end
 end

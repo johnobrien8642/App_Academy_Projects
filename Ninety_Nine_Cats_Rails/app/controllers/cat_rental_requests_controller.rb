79f708b4b2938924c495bb9765ef0d9ce8afff1d
your name here <johnobrien8642@gmail.com>
2020-10-27 15:24:21 -0400

    ::CatRentalRequests #approve, #deny created, show.html.erb shows Approve Deny buttons when status = pending, hidden when not

--- Ninety_Nine_Cats_Rails/app/controllers/cat_rental_requests_controller.rb
+++ Ninety_Nine_Cats_Rails/app/controllers/cat_rental_requests_controller.rb
@@ -1,7 +1,5 @@
 class CatRentalRequestsController < ApplicationController
     
-    
-    
     def create
       @rental_request = CatRentalRequest.new(cat_rental_request_params)
       if @rental_request.save!
@@ -14,6 +12,18 @@ class CatRentalRequestsController < ApplicationController
     
     def new
       @rental_request = CatRentalRequest.new
+    end
+
+    def approve
+      @rental_request = CatRentalRequest.find(params[:id])
+      @rental_request.approve!
+      redirect_to cat_url(@rental_request.cat)
+    end
+
+    def deny
+      @rental_request = CatRentalRequest.find(params[:id])    
+      @rental_request.deny!
+      redirect_to cat_url(@rental_request.cat)
     end
 
     private

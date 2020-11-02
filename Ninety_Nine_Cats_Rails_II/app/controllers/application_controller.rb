3b581979f5a319596ac9fc235e7b857ce5802814
your name here <johnobrien8642@gmail.com>
2020-11-02 16:15:47 -0500

    views/cats/index.html.erb updated from NNCI w/ css, font link added to layouts/application.html.erb, seeds added, ::ApplicationController#login_user! fixed and working

--- Ninety_Nine_Cats_Rails_II/app/controllers/application_controller.rb
+++ Ninety_Nine_Cats_Rails_II/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
     def login_user!(user)
-      session.session_token = user.reset_session_token!
+      session[:session_token] = user.reset_session_token!
     end
 
 end
--- Ninety_Nine_Cats_Rails_II/db/seeds.rb
+++ Ninety_Nine_Cats_Rails_II/db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+c1 = Cat.create(birth_date: "Nov 4th 2015", color: "brown", 
+                name: "Sprinkles", sex: "F", 
+                description: "A nice cat, probably too nice"
+            )
+c2 = Cat.create(birth_date: "Nov 5th 2015", color: "white", 
+                name: "Cinnamon", sex: "F", 
+                description: "This cat loves to jump"
+            )
+c3 = Cat.create(birth_date: "Nov 6th 2016", color: "black", 
+                name: "Furball", sex: "M", 
+                description: "Man this cat sure has a lot of fur"
+            )
+c4 = Cat.create(birth_date: "Nov 7th 2011", color: "orange", 
+                name: "Tinkerbell", sex: "F", 
+                description: "This is an outdoor cat"
+            )
+c5 = Cat.create(birth_date: "Jan 1st 2018", color: "brown", 
+                name: "Cato", sex: "M", 
+                description: "A young vibrant cat"
+            )
+
+CatRentalRequest.create(cat_id: c1.id, start_date: "June 1st 2021", 
+                            end_date: "June 30th 2021", status: :Approved
+                        )
+
+CatRentalRequest.create(cat_id: c2.id, start_date: "July 5th 2021", 
+                            end_date: "August 8th 2021", status: :Approved
+                        )                        
+
+CatRentalRequest.create(cat_id: c1.id, start_date: "February 20th 2021", 
+                            end_date: "March 5th 2021", status: :Pending
+                        )
+
+CatRentalRequest.create(cat_id: c4.id, start_date: "April 5th 2021", 
+                            end_date: "April 10th 2021", status: :Denied
+                        )
+
+CatRentalRequest.create(cat_id: c5.id, start_date: "October 1st 2021", 
+                            end_date: "October 30th 2021", status: :Pending
+                        )

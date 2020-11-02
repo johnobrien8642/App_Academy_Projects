33cee6035a4b74cf054e79490891a27b436be4d4
your name here <johnobrien8642@gmail.com>
2020-11-02 14:57:40 -0500

    ::URL#reset_session_token! ::generate_session_token created, validations username, session_token, presence uniqueness true, password_digest presence w/ message

--- Ninety_Nine_Cats_Rails_II/app/models/user.rb
+++ Ninety_Nine_Cats_Rails_II/app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :password_digest, presence: { message: "Password can't be blank"}
+    validates :session_token, presence: true, uniqueness: true
+
+    def reset_session_token!
+      self.session_token = self.class.generate_session_token
+    end
+
+    def self.generate_session_token
+      SecureRandom::urlsafe_base64(16)
+    end
 end

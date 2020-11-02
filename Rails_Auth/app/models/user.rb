0cf86855e0bc05f48455011c3b820fb90d7885fe
your name here <johnobrien8642@gmail.com>
2020-11-01 19:07:12 -0500

    ::User validations tested and cleaned, all working

--- Rails_Auth/app/models/user.rb
+++ Rails_Auth/app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     attr_reader :password
 
-    validates :username, :session_token presence: true
-    validates :password_digest, presence: {message: "Password can't be blank"} 
+    validates :username, :session_token, presence: true
+    validates :password_digest, presence: { message: "Password can't be blank" } 
     validates :username, :session_token, uniqueness: true
-    validates :password, { minimum: 6, allow_nil: true }
+    validates :password, length: { minimum: 6, allow_nil: true }
 
     def password=(password)
       @password = password

e5273e06e86f1221a821abf8b16642c076c22bf4
your name here <johnobrien8642@gmail.com>
2020-11-01 20:17:13 -0500

    ::User::find_by_credentials ::generate_session_token #reset_session_token! #ensure_session_token created

--- Rails_Auth/app/models/user.rb
+++ Rails_Auth/app/models/user.rb
@@ -14,4 +14,24 @@ class User < ApplicationRecord
     def is_password?(password)
       BCrypt::Password.new(self.password_digest).is_password?(password)
     end
+
+    def self.find_by_credentials(username, password)
+      user = User.find_by(username: username)
+      return user if user && BCrypt::Password.new(user.password_digest).is_password?(password)
+      nil
+    end
+
+    def self.generate_session_token
+      SecureRandom::urlsafe_base64
+    end
+
+    def reset_session_token!
+      self.session_token = User.generate_session_token
+      self.save!
+      self.session_token
+    end
+
+    def ensure_session_token
+      self.session_token ||= User.generate_session_token
+    end
 end

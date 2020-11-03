2d7e61d4e5037a35e66a8cbd55ae0b0012a4167c
your name here <johnobrien8642@gmail.com>
2020-11-02 19:09:38 -0500

    re-added solution again, ::UsersController before_action :require_no_user! ::SessionsController before_action :require_no_user only :create, :new created, #destroy working

--- Ninety_Nine_Cats_Rails_II/app/controllers/users_controller.rb
+++ Ninety_Nine_Cats_Rails_II/app/controllers/users_controller.rb
@@ -1,13 +1,14 @@
 class UsersController < ApplicationController
+  before_action :require_no_user!
   
   def create
     @user = User.new(user_params)
 
-    if @user.save!
+    if @user.save
       login_user!(@user)
       redirect_to cats_url
     else
-      flash.now[:errors] = ["Username and/or password incorrect"]
+      flash.now[:errors] = @user.errors.full_messages 
       render :new
     end
   end
--- Ninety_Nine_Cats_Rails_II/app/models/user.rb
+++ Ninety_Nine_Cats_Rails_II/app/models/user.rb
@@ -24,18 +24,14 @@ class User < ApplicationRecord
     end
     
     def reset_session_token!
-       self.session_token = User.generate_session_token
+       self.session_token = SecureRandom.urlsafe_base64(16)
        self.save!
        self.session_token
     end
 
-    def self.generate_session_token
-      SecureRandom::urlsafe_base64(16)
-    end
-    
     private
     
     def ensure_session_token
-      self.session_token ||= User.generate_session_token
+      self.session_token ||= SecureRandom.urlsafe_base64(16)
     end
 end

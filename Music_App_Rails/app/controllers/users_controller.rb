e1a6930e8f117e7049faad41219eaf78206053cb
your name here <johnobrien8642@gmail.com>
2020-11-06 21:34:09 -0500

    User and Session controllers, views
    Basic user [new show create] and session [new show create destroy]
    controllers with corresponding views created. All views currently
    working and interacting with help of ApplicationController [login_user!
    current_user logged_in logout_user!].

--- Music_App_Rails/app/controllers/users_controller.rb
+++ Music_App_Rails/app/controllers/users_controller.rb
@@ -8,15 +8,19 @@ class UsersController < ApplicationController
     def create
       user = User.new(user_params)
 
-      if user.save!
+      if user.save
         login_user!(user)
-       #???? redirect_to _url
+        redirect_to user_url(user)
       else
         flash.now[:errors] = user.errors.full_messages
         render :new
       end
     end
 
+    def show
+      @user = User.find(params[:id])
+      render :show
+    end
 
     private
 
--- Music_App_Rails/db/seeds.rb
+++ Music_App_Rails/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+u1 = User.new(email:"amigo@badhombre.com", password:"ayepapi")
+u2 = User.new(email:"howabowdat@cashmeoutside.com", password:"badbhabi")
+u3 = User.new(email:"joejonas@thejonasbros.com", password:"tiddlywinks") 

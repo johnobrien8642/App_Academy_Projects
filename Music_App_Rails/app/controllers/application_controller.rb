d3bb8305def6aed5ecf92b11ed686bdfc673ca06
your name here <johnobrien8642@gmail.com>
2020-11-09 15:28:53 -0500

    Basic views with stylesheets
    Finished views, adding classes for css styling. Only views for
    models up to this point have been created, giving a very Basic
    foundation for furthering style.

--- Music_App_Rails/app/controllers/application_controller.rb
+++ Music_App_Rails/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ require 'byebug'
 class ApplicationController < ActionController::Base
 
     helper_method :current_user
+    helper_method :logged_in?
 
     def login_user!(user)
       session[:session_token] = user.reset_session_token!

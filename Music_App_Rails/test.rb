dd47d50c77d67e06938a4712906f38292722a4bd
your name here <johnobrien8642@gmail.com>
2021-01-08 14:37:58 -0500

    advanced-rails-forms merge to master

---
+++ Music_App_Rails/test.rb
@@ -0,0 +1,16 @@
+module Enumerable
+  protected def ins(element)
+    return [element] if count.zero?
+
+    head = first
+    return [element] + self if element <= head
+
+    [head] + drop(1).ins(element)
+  end
+
+  def stable_sort
+    return [] if count.zero?
+
+    drop(1).stable_sort.ins(first)
+  end
+end

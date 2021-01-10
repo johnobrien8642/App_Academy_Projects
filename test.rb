7194e9e00d69f0acbdfb00a58f4027b2706d3213
your name here <johnobrien8642@gmail.com>
2021-01-09 19:13:21 -0500

    Rails_Lite

--- test.rb
+++ test.rb
@@ -1,10 +1,12 @@
+require 'byebug'
+
 module Enumerable
   protected def ins(element)
     return [element] if count.zero?
 
     head = first
     return [element] + self if element <= head
-
+    debugger
     [head] + drop(1).ins(element)
   end
 

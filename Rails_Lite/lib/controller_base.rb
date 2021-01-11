b26abaaba7d9ee2b57d261f8738e93c5161fcd8d
your name here <johnobrien8642@gmail.com>
2021-01-10 20:37:29 -0500

    Rails_Lite_Pt_2
    Skipped over final bonus tasks

--- Rails_Lite/lib/controller_base.rb
+++ Rails_Lite/lib/controller_base.rb
@@ -7,7 +7,7 @@ require_relative './session'
 require 'byebug'
 
 class ControllerBase
-  attr_reader :req, :res, :params
+  attr_reader :req, :res, :params, :auth_token
 
   # Setup the controller
   def initialize(req, res, route_params = {})
@@ -15,6 +15,7 @@ class ControllerBase
     @res = res
     @params = route_params.merge(req.params) 
     @already_built_response = false
+    @@protect_from_forgery ||= false
   end
 
   # Helper method to alias @already_built_response
@@ -68,16 +69,48 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    if protect_from_forgery? && req.request_method != "GET"
+      check_authenticity_token
+    else
+      form_authenticity_token
+    end  
+    
+    self.send(name)
+    render(name) unless already_built_response?
   
-   self.send(name)
-   render(name) unless already_built_response?
-  
+    nil
+  end
+
+  def self.protect_from_forgery
+    @@protect_from_forgery = true
+  end
+
+  def protect_from_forgery?
+    @@protect_from_forgery
   end
 
   def prepare_render_or_redirect
     raise "double render error" if already_built_response?
     @already_built_response = true
     session.store_session(@res)
+  end
+
+  def form_authenticity_token
+    @auth_token ||= generate_random_token
+    @res.set_cookie('authenticity_token', value: @auth_token, path: '/')
+    @auth_token
+  end 
+
+  def check_authenticity_token
+    cookie_auth_token = @req.cookies['authenticity_token']
+    unless cookie_auth_token && cookie_auth_token == 
+        params['authenticity_token']
+      raise 'Invalid authenticity token'
+    end
+  end
+
+  def generate_random_token
+    SecureRandom::urlsafe_base64(16)
   end
 end
 
--- Rails_Lite/lib/flash.rb
+++ Rails_Lite/lib/flash.rb
@@ -1,4 +1,28 @@
 require 'json'
 
 class Flash
+	
+	def initialize(req)
+		cookie = req.cookies['_rails_lite_app_flash']
+
+		@now = cookie ? JSON.parse(cookie) : {}
+    @flash = {}
+	end
+	
+	def [](key)
+		@now[key.to_s] || @flash[key.to_s]
+	end
+
+	def []=(key, val)
+		@flash[key.to_s] = val
+	end
+
+	def now
+		@now
+	end
+
+	def store_flash(res)
+		cookie = { path: "/", value: @flash.to_json }
+		res.set_cookie('_rails_lite_app_flash', cookie)
+	end
 end

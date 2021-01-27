2b1766c3dca7a9a4da59ccb0ebd9a931faa4a260
your name here <johnobrien8642@gmail.com>
2021-01-27 18:49:33 -0500

    Configure_NPM Redux_Lite

--- React/Redux_Lite/phase3/redux.js
+++ React/Redux_Lite/phase3/redux.js
@@ -1,12 +1,22 @@
 class Store {
-  constructor(rootReducer) {
+  
+  constructor(rootReduceer) {
     this.rootReducer = rootReducer;
     this.state = {};
     this.getState = this.getState.bind(this);
+    this.subscriptions = [];
   }
 
   getState() {
     return Object.assign({}, this.state);
+  }
+
+  dispatch(action) {
+    this.state = this.rootReducer(this.state, action);
+  }
+
+  subscribe(cb) {
+    this.subscriptions.push(cb);
   }
 }
 
@@ -16,8 +26,13 @@ const combineReducers = config => {
   return (prevState, action) => {
     const nextState = {};
     Object.keys(config).forEach(k => {
-      nextState[k] = config[k](prevState[k], action);
+      if (!action) {
+        nextState[k] = config[k](...args);
+      } else {
+        nextState[k] = config[k](prevState[k], action);
+      }
     });
     return nextState;
   }
 }
+

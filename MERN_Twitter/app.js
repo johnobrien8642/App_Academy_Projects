b5965e9e9f1514e12e56e00394a5c3141f3d53f1
your name here <johnobrien8642@gmail.com>
2021-02-16 13:46:55 -0500

    MERN_Twitter node mongo express init

---
+++ MERN_Twitter/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const db = require('./config/keys').mongoURI
+const mongoose = require('mongoose')
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+app.get("/", (req, res) => res.send("Hello World!!!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
+
+const users = require("./routes/api/users")
+const tweets = require("./routes/api/tweets")
---
+++ MERN_Twitter/config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: `mongodb+srv://dev:UsBWoJncB2XJNs8d@cluster0.jfsaa.mongodb.net/test?retryWrites=true&w=majority`
+}
